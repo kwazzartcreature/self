8ba193f0eda465ab1da3171aa6ed04ff71a325cf
kwazzartcreature <kwazzartcreature@gmail.com>
2025-01-19 12:42:21 +0300

    init project

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  i18n: {
+    routing: {
+      prefixDefaultLocale: false,
+    },
+    locales: ["en", "ru"],
+    defaultLocale: "en",
+  },
+  integrations: [svelte(), tailwind()],
+});
